75bb1e74564a44a2693d685943dbea1ec3c4cf73
ndvam <ndvamr@gmail.com>
2025-11-25 10:53:50 +0200

    constraints definition finish

--- main.py
+++ main.py
@@ -40,15 +40,37 @@ class CSP:
         """
         constraints = {}
         n = len(variables)
-
+        # --- Part 1 Decide about constrained variables---
         # Max number of constraints possible:
         max_constraints = (n * (n - 1)) // 2
         # Define number of constraints:
         num_constraints = int(max_constraints * density)
-        # Choose random pairs:
+        # Choose random pairs to be constrained:
         all_possible_pairs = list(combinations(variables, 2))
         chosen_pairs = random.sample(all_possible_pairs, num_constraints)
+
+        # --- Part 2 Decide about constrained values---
+        # Get domain size
+        domain_values = domains[variables[0]]
+        domain_size = len(domain_values)
+        # All value pairs possible - cartesian multiplication:
+        all_value_pairs = [(v1, v2) for v1 in domain_values for v2 in domain_values]
+        # Get num of conflicts:
+        max_conflicts = domain_size * domain_size
+        num_conflicts = int(max_conflicts * tightness)
+
+        for (var1, var2) in chosen_pairs:
+            # Get Random pairs of values
+            forbidden_pairs = set(random.sample(all_value_pairs, num_conflicts))
+
+            # Save the original constrain
+            constraints[(var1, var2)] = forbidden_pairs
+
+            # Save the opposite constrain as well
+            reversed_forbidden_pairs = {(v2, v1) for (v1, v2) in forbidden_pairs}
+            constraints[(var2, var1)] = reversed_forbidden_pairs
+
         return constraints
 
 if __name__ == "__main__":
-    mycsp = CSP(n_var=6,domain_size=2,density=0.1,tightness=0.1)
+    mycsp = CSP(n_var=3,domain_size=3,density=0.34,tightness=0.33)
